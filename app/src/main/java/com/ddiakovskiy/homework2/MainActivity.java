57bb5f96a0afa8e5282d9a7ecd5f95adbab7d0fa
Diakovskiy <ganwhite@gmail.com>
2017-10-17 00:34:02 +0300

    added activity execution

--- app/src/main/java/com/ddiakovskiy/homework2/MainActivity.java
+++ app/src/main/java/com/ddiakovskiy/homework2/MainActivity.java
@@ -1,7 +1,11 @@
 package com.ddiakovskiy.homework2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickButtonCall(View v){
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:"));
+        startActivity(intent);
+    }
+    public void onClickButtonApp(View v){
+        Intent appIntent = new Intent(this, SecondActivity.class);
+        startActivity(appIntent);
+    }
+    public void onClickButtonSms(View v){
+
+        Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+        sendIntent.setData(Uri.parse("sms:"));
+        startActivity(sendIntent);
+
+    }
+
+
+
+
 }
